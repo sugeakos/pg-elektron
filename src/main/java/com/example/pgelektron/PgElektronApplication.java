328f2d944eceb2f3ceb39baa550a6587ed7c1015
sugeakos <sugeakos@gmail.com>
2022-03-19 15:01:47 +0100

    init project

---
+++ src/main/java/com/example/pgelektron/PgElektronApplication.java
@@ -0,0 +1,13 @@
+package com.example.pgelektron;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PgElektronApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(PgElektronApplication.class, args);
+    }
+
+}
